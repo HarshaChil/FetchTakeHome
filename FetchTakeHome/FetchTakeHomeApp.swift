4416a59f2194e7cb14a4135a7eac11a67ca6cb0f
Harsha Chilakapati <134959979+HarshaChil@users.noreply.github.com>
2024-10-11 00:00:28 -0500

    Add Item and Data struct
    Item contains necessary info for 1 item and Data contains the entire dataset

--- FetchTakeHome/FetchTakeHomeApp.swift
+++ FetchTakeHome/FetchTakeHomeApp.swift
@@ -7,6 +7,27 @@
 
 import SwiftUI
 
+struct Item : Codable{
+    let id: Int
+    let listId: Int
+    let name: String?
+}
+
+struct Data {
+    var items: [Item] = []
+    mutating func getData() {
+        guard let url = URL(string: "https://fetch-hiring.s3.amazonaws.com/hiring.json") else{ return }
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
+            guard let error else { return }
+            guard let data else { return }
+            let items = try? JSONDecoder().decode([Item].self, from: data)
+            let validItems = items?.filter { $0.name != nil && !$0.name!.isEmpty } ?? []
+            self.items = validItems.sorted{($0.listId, $0.name!) < (($1.listId, $1.name!))}
+        }
+        task.resume()
+    }
+}
+
 @main
 struct FetchTakeHomeApp: App {
     var body: some Scene {
