9edec87e60fe5ed001d48bcd18bf6705b0631661
Harsha Chilakapati <134959979+HarshaChil@users.noreply.github.com>
2024-10-11 00:05:22 -0500

    Fixed closure capturing mutating self error

--- FetchTakeHome/FetchTakeHomeApp.swift
+++ FetchTakeHome/FetchTakeHomeApp.swift
@@ -16,14 +16,15 @@ struct Item : Codable{
 struct Data {
     var items: [Item] = []
     mutating func getData() {
+        var validItems : [Item] = []
         guard let url = URL(string: "https://fetch-hiring.s3.amazonaws.com/hiring.json") else{ return }
         let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
             guard let error else { return }
             guard let data else { return }
             let items = try? JSONDecoder().decode([Item].self, from: data)
-            let validItems = items?.filter { $0.name != nil && !$0.name!.isEmpty } ?? []
-            self.items = validItems.sorted{($0.listId, $0.name!) < (($1.listId, $1.name!))}
+            validItems = items?.filter { $0.name != nil && !$0.name!.isEmpty } ?? []
         }
+        self.items = validItems
         task.resume()
     }
 }
