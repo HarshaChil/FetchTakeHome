a800c3d0b8c328368d7fbfb35676f71ef33e6555
Harsha Chilakapati <134959979+HarshaChil@users.noreply.github.com>
2024-10-10 23:05:57 -0500

    Initial Commit

---
+++ FetchTakeHome/FetchTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchTakeHomeApp.swift
+//  FetchTakeHome
+//
+//  Created by Harsha Chilakapati on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FetchTakeHomeTests/FetchTakeHomeTests.swift
@@ -0,0 +1,17 @@
+//
+//  FetchTakeHomeTests.swift
+//  FetchTakeHomeTests
+//
+//  Created by Harsha Chilakapati on 10/10/24.
+//
+
+import Testing
+@testable import FetchTakeHome
+
+struct FetchTakeHomeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
